76db8c9e48658698ec6cc35a1ed0836fd7a24e1f
VaishaliQA <patelvaishali1890@gmail.com>
2022-12-05 17:09:29 -0800

    created mongodb cluster

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
+//const uri = "mongodb+srv://testdb:testdb@<your-cluster-url>/test?retryWrites=true&w=majority";
 
 mongoose.connect(
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
